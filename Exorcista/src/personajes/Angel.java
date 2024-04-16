9076171eda2e349e2a2f6ad79d76d2b7e6192fcb
CokeinsZ <stivencarvajal205@gmail.com>
2024-04-15 20:10:30 -0500

    pasos inciales para dibujar el angel en la ventana pricipal
    Se creó un JFrame y se dibuja el angel en la ventana

--- Exorcista/src/personajes/Angel.java
+++ Exorcista/src/personajes/Angel.java
@@ -5,16 +5,45 @@
 package personajes;
 
 import ayudas.Potenciador;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
 
 /**
  *
  * @author Alejandro
  */
-public class Angel {
-
+public class Angel extends Rectangle{
+    
     private float vida;
     private float energia;
-    private int[] posicion;
     private Potenciador[] potenciadores;
     private int almasLiberadas;
+    
+    private Image imagenFondo;
+    private void cargarImagen
+
+    public Angel(int x, int y) {
+        super(x, y, 100, 100);
+        
+        this.vida = 100;
+        this.energia = 100;
+        this.potenciadores = new Potenciador[3];    //El jugador va a poder tener máximo 3 potenciadores
+        this.almasLiberadas = 0;
+    }
+    
+    public Angel() {
+        super(0, 0, 100, 100);
+        
+        this.vida = 100;
+        this.energia = 100;
+        this.potenciadores = new Potenciador[3];    //El jugador va a poder tener máximo 3 potenciadores
+        this.almasLiberadas = 0;
+    }
+    
+    public void dibujarAngel (Graphics2D g) {
+        g.drawImage(this.imagenFondo, 0, 0, null);
+        g.draw(this);
+    }
+    
 }
