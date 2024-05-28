67d979d0e43f55c6cb2922ef5da81d6e731dbd51
victormanuel20 <victormanuel20034@gmail.com>
2024-05-27 22:59:20 -0500

    implemente aalgo en tornado

---
+++ Exorcista/src/personajes/poderDemonios/Tornado.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package personajes.poderDemonios;
+
+import interfaces.Notificable;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import personajes.Angel;
+import sprite.Dibujo;
+
+/**
+ *
+ * @author usuario
+ */
+public class Tornado extends Dibujo {
+    
+    public static final int ANCHO = 50;
+    public static final int ALTO = 50;
+    public static final int DAÑO = 10;
+
+    private int velocidad = 4;
+    private Notificable notificador;
+    private volatile boolean seLlego;
+
+    private int objetivoX;
+    private int objetivoY;
+
+    private Angel objetivo;
+
+    public Tornado(int x, int y, Image imagen, Notificable notificador, Angel objetivo) {
+        super(x, y, ANCHO, ALTO, imagen);
+        seLlego = false;
+
+        this.notificador = notificador;
+        this.objetivo = objetivo;
+
+    }
+
+    @Override
+    public void dibujar(Graphics2D g) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
